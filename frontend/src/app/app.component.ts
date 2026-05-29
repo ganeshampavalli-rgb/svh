3c7b365c2625f55eb1dfcd40500a12cc40a644d7
Ganesh Ampavalli <ganesh.ampavalli@gmail.com>
2026-05-29 13:19:23 +0530

    Direct HomeComponent load - remove routing complexity from root app

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './features/home/components/home/home.component';
 
 @Component({
   selector: 'svh-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HomeComponent],
   template: `
-    <router-outlet></router-outlet>
+    <svh-home></svh-home>
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Sri Venkateswara Home Tutions';
 }
+
+
 
